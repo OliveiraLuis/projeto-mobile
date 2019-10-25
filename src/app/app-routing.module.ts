9a6f7e61cfd92b5b871e9dadae30f2a471c9befb
João Victor <jvictor2012@outlook.com>
2019-10-24 22:16:46 -0300

    Login e registro de usuários
    Funcionando corretamente, efetuado teste no firebase, está adicionando la já.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+ // { path: '', redirectTo: 'home', pathMatch: 'full' },
+  //{ path: '', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'buscar', loadChildren: './buscar/buscar.module#BuscarPageModule' },
   { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
 ];
