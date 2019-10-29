8804b20a7afb1915fd69b415d95f9c01ea8425e1
João Victor <jvictor2012@outlook.com>
2019-10-29 19:38:55 -0300

    Protótipo da página de playlist pronto

--- src/app/playlist/playlist.page.ts
+++ src/app/playlist/playlist.page.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaylistPage implements OnInit {
 
+  playlist: any;
   constructor() { }
 
   ngOnInit() {
+
+    this.playlist = [
+      {
+        name: "Playlist1",
+        author: "Nome do usuário",
+        thumbnail: "../assets/icon/favicon.png",
+        description: "Músicas que me fazer relaxar."
+      },
+      {
+        name: "Playlist2",
+        author: "Nome do usuário",
+        thumbnail: "../assets/icon/favicon.png",
+        description: "Músicas que me fazer ficar alegre."
+      },
+      {
+        name: "Playlist3",
+        author: "Nome do usuário",
+        thumbnail: "../assets/icon/favicon.png",
+        description: "Músicas que me fazer dormir."
+      },
+    ]
+
   }
 
 }
